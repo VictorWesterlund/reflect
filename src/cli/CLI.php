c8a1adc7b4cebacfe60147df39f9a2259a69d295
Victor Westerlund <victor.vesterlund@gmail.com>
2023-01-05 16:23:54 +0100

    feat: socket server cli refactor (#14)

--- src/cli/CLI.php
+++ src/cli/CLI.php
@@ -1,10 +1,28 @@
 <?php
 
+    if (php_sapi_name() !== "cli") {
+        die("Must be run from command line");
+    }
+
     class CLI {
-        public function __construct() {
-            if (php_sapi_name() !== "cli") {
-                die("Must be run from command line");
+        public function __construct(array $args, int $arglen) {
+            // Get all CLI args except name of script file
+            array_shift($args);
+            $this->args = $args;
+
+            // Pad array of arguments to expected length with nulls
+            if (count($this->args) < $arglen) {
+                $this->pad_args($arglen);
             }
+        }
+
+        // Add padding to argument array
+        private function pad_args(int $target_arglen) {
+            $arglen = count($this->args);
+            $padding = $target_arglen - $arglen;
+
+            // Fill remaining array slots with nulls
+            $this->args = array_merge($this->args, array_fill($arglen, $padding, null));
         }
 
         public function echo(mixed $msg) {
--- src/cli/socket/SocketServer.php
+++ src/cli/socket/SocketServer.php
@@ -1,33 +1,28 @@
 <?php
 
-    require_once "src/Init.php";
     require_once Path::src("request/Router.php");
 
     // Handle RESTful requests over AF_UNIX socket.
     class SocketServer {
-        public function __construct() {
-            $this->init();
-
-            // Create and bind socket file
-            $this->socket = socket_create(AF_UNIX, SOCK_STREAM, 0);
-            socket_bind($this->socket, $_ENV["socket"]["listen"]);
-        }
-
-        // Initialize server
-        private function init() {
+        public function __construct(string $path) {
             // Can not access socket file
-            if (!is_writable($_ENV["socket"]["listen"])) {
-                throw new Error("No permission: Cannot create .sock file at '{$_ENV["socket"]["listen"]}'");
+            if (!is_writable(dirname($path))) {
+                throw new Error("No permission: Cannot create socket file at '{$path}'");
             }
 
             // Delete existing socket file
-            if (file_exists($_ENV["socket"]["listen"])) {
-                unlink($_ENV["socket"]["listen"]);
+            if (file_exists($path)) {
+                unlink($path);
             }
+
+            // Create and bind socket file
+            $this->socket = socket_create(AF_UNIX, SOCK_STREAM, 0);
+            socket_bind($this->socket, $path);
         }
 
         // Convert message from client into simple PHP-styled presentation of HTTP request
         private function parse(string $payload) {
+            // Expecting ["<endpoint>","<method>","<payload>"]
             [$uri, $_SERVER["REQUEST_METHOD"], $data] = json_decode($payload);
 
             // Request is malformed or connection interrupted, abort
@@ -61,7 +56,7 @@
             while ($con === true) {
                 $client = socket_accept($this->socket);
 
-                //Bind handler for outgoing data
+                // Bind handler for outgoing data
                 $_ENV["SOCKET_STDOUT"] = function (string $msg, int $code = 200) use (&$client) {
                     $tx = json_encode([$code, $msg]);
                     socket_write($client, $tx, strlen($tx));
